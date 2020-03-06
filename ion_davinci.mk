d37d8d499c390c35ebe2dea052efaacb3113186a
Laker <61315154+Laker87@users.noreply.github.com>
2020-03-06 13:43:53 +0300

    ion

--- ion_davinci.mk
+++ ion_davinci.mk
@@ -8,13 +8,13 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/ion/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := davinci
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := xtended_davinci
+PRODUCT_NAME := ion_davinci
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
